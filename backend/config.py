55882e1ab120c78f1b9008c84ccc2942c406888e
Mahfujur <xmahfujur@gmail.com>
2026-04-07 07:53:08 +0600

    del

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-from google import genai
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
--- backend/rag/loader.py
+++
@@ -1,9 +0,0 @@
-from llama_index.core import SimpleDirectoryReader
-
-def document_loader(path : str):
-    docs = SimpleDirectoryReader(path).load_data()
-    return docs
-
-data = document_loader('backend/uploads')
-# print(data[0].text[:1000])
-    
